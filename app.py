4b004bd0035a4c48c9a3445d81d3a6a925e77661
chandana-24 <chandanahb2005@gmail.com>
2026-05-17 21:10:33 +0530

    Implement button click action in Streamlit app
    Added button functionality to trigger an action.

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-import streamlit as st
+mport streamlit as st
 
 st.title("My AI App 🚀")
 st.write("Welcome to my Streamlit website")
 st.write("this is my first App creation project!")
-st.button("click")
+
+# Connect the button to an action using an 'if' statement
+if st.button("click"):
+    # Everything indented under here happens ONLY when the button is clicked
+    st.success("You clicked the button! It works!")
+    st.balloons() # This adds a fun animation!
